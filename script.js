65c598acc66ad31928b4965296b93200ea49a225
Lucas Skog <lucasskog@gmail.com>
2024-01-25 22:59:52 +0100

    Refactoring to be able to solve last exercise

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ import { icons, selectors } from "./constants.js";
 const addNew = document.querySelector("button");
 const input = document.querySelector("input");
 const container = document.querySelector(".container");
+const todoCards = [];
 let currentAuthor;
-addNew.addEventListener("click", () => {    
-    currentAuthor = document.querySelector("option:checked");
-   
-    if(!currentAuthor.innerText.includes("--")){
-        newTodoItem(input.value);
-        input.value = "";
-    } else{
-        alert("Choose an author..");
-    }
+addNew.addEventListener("click", () => {
+  currentAuthor = document.querySelector("option:checked");
+
+  if (!currentAuthor.innerText.includes("--")) {
+    newTodoItem(input.value);
+    input.value = "";
+  } else {
+    alert("Choose an author..");
+  }
 });
 
 newTodoItem("Plocka äpplen");
@@ -21,50 +22,52 @@ newTodoItem("Dricka kaffe");
 
 attachIconListeners();
 function attachIconListeners() {
-    document.querySelectorAll("span").forEach((icon) => {
-        icon.removeEventListener("click", handleSpanClick);
-    });
-    document.querySelectorAll("span").forEach((icon) => {    
-        icon.addEventListener("click", handleSpanClick);
-    });
+  document.querySelectorAll("span").forEach((icon) => {
+    icon.removeEventListener("click", handleSpanClick);
+  });
+  document.querySelectorAll("span").forEach((icon) => {
+    icon.addEventListener("click", handleSpanClick);
+  });
 
-    document.querySelectorAll("i").forEach((edit) => {
-        edit.removeEventListener("click", handleEditClick);
-    });
-    document.querySelectorAll("i").forEach((edit) => {
-        edit.addEventListener("click", handleEditClick);
-    });
+  document.querySelectorAll("i").forEach((edit) => {
+    edit.removeEventListener("click", handleEditClick);
+  });
+  document.querySelectorAll("i").forEach((edit) => {
+    edit.addEventListener("click", handleEditClick);
+  });
 }
 
 function handleSpanClick(e) {
-    const todoItem = e.target.closest(".todo-item");
-    switch (e.target.id) {
-        case "upArrow":
-            tryMoveUp(todoItem);
-            break;
-        case "downArrow":
-            tryMoveDown(todoItem);
-            break;
-        case "delete":
-        case "done":
-            todoItem.remove();
-            break;
-    }
+  const todoItem = e.target.closest(".todo-item");
+  switch (e.target.id) {
+    case "upArrow":
+      tryMoveUp(todoItem);
+      break;
+    case "downArrow":
+      tryMoveDown(todoItem);
+      break;
+    case "delete":
+    case "done":
+      todoItem.remove();
+      break;
+  }
 }
 
 function handleEditClick(e) {
-    const taskText = e.target.nextElementSibling;
-    taskText.disabled = false;    
-    taskText.addEventListener("input", (key) => {
-        if(key.key === "Enter") {
-            taskText.blur();
-        } else {
-            taskText.style.width = (taskText.value.length + 1) * 8 + "px";
-            if((taskText.value.length + 1) * 8 > 185){
-                taskText.style.width = "185px";
-            }
-        }
-    });
+  const taskText = e.target.nextElementSibling;
+  taskText.disabled = false;
+  taskText.addEventListener("input", (key) => {
+    if (key.key === "Enter") {
+      taskText.blur();
+    } else {
+      taskText.style.width = getInputWidth(taskText.value.length);
+    }
+  });
+}
+
+function getInputWidth(length) {
+  let width = (length + 1) * 8;
+  return width > 185 ? "185px" : `${width}px`;
 }
 
 function tryMoveUp(element) {
@@ -81,61 +84,56 @@ function tryMoveDown(element) {
   }
 }
 
-function newTodoItem(task) {
-  let childElements = [];
-  for (let key in selectors) {
-    let currElement = createElement(
-      selectors[key].element,
-      selectors[key].className
-    );
-    switch (key) {
-      case "todoCard":
-        childElements.reverse();
-        childElements.forEach((element) => currElement.appendChild(element));
-        container.appendChild(currElement);
-        break;
-      case "textBox":
-        childElements.forEach((element) => currElement.appendChild(element));
-        childElements.length = 0;
-      case "author":     
-     if(currentAuthor)
-             childElements.unshift(document.createTextNode("By: "+ currentAuthor.innerText+ ", "))
-            currentAuthor = null;
-          childElements.forEach((element) => currElement.appendChild(element));
-          childElements.length = 0;
-          
-        break;
-      case "task":
-        currElement.value = task;
-        currElement.disabled = true;
-        currElement.style.width = (currElement.value.length + 1) * 8 + "px";
-        const bla = currElement;
-        currElement = createElement("label", "edit");
-        currElement.innerHTML = `<i class="material-symbols-outlined">
-            edit
-            </i>`;
-        currElement.appendChild(bla);
-        break;
-      case "created":
-        currElement.innerText = getCurrentDateTime();
-        break;
-      case "icons":
-        Object.values(icons).forEach(
-          (icon) => (currElement.innerHTML += icon.html)
-        );
+function newTodoItem(text) {
+  const task = createElement("input", "task");
+  task.setAttribute("disabled", "true");
+  task.setAttribute("value", text);
+  task.value = text;
+  task.style.width = getInputWidth(task.value.length);
+  let currDate = getCurrentDateTime(),
+    currAuthor = getAuthor();
+  const taskDiv = createElement("div", "todo-item");
+  taskDiv.innerHTML = todoCard(task.outerHTML, currAuthor, currDate);
 
-        break;
-      default:
-    }
-    childElements.unshift(currElement);
-  }
-  attachIconListeners();
+  const iconsDiv = createElement("div", "icons");
+  Object.values(icons).forEach((icon) => (iconsDiv.innerHTML += icon.html));
+
+  taskDiv.appendChild(iconsDiv);
+
+  let todoTask = {
+    card: taskDiv,
+    author: currAuthor,
+    date: currDate,
+  };
+  todoCards.push(todoTask);
+
+  container.appendChild(taskDiv);
+}
+
+function todoCard(inputelement, author, date) {
+  return `<div class="task-item">
+<label class="edit">
+<i class="material-symbols-outlined"> edit </i>
+${inputelement}
+</label>
+<p class="author"> ${author}
+<span class="date">${date}</span>
+</p>
+</div>`;
 }
 
 function createElement(element, className) {
   return Object.assign(document.createElement(element), {
     classList: className,
   });
+}
+
+function getAuthor() {
+  if (currentAuthor) {
+    return currentAuthor.innerText;
+  } else {
+    return "";
+  }
 }
 
 function getCurrentDateTime() {
